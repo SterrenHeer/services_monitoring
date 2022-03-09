668db96d973fa08bf7892db06c03875f185a4ab3
SterrenHeer <sterrenheer@gmail.com>
2022-03-09 11:12:56 +0300

    Added requests models

--- requests/admin.py
+++ requests/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import RepairRequest
+from .models import RepairRequest, RequestComment, Comment, Recommendation
 
 admin.site.register(RepairRequest)
+admin.site.register(RequestComment)
+admin.site.register(Comment)
+admin.site.register(Recommendation)
--- requests/models.py
+++ requests/models.py
@@ -12,3 +12,34 @@ class RepairRequest(models.Model):
 
     def __str__(self):
         return f"{self.service}"
+
+
+class RequestComment (models.Model):
+    comment_text = models.CharField(max_length=200, help_text="Введите комментарий")
+    status = models.CharField(max_length=200, default="На рассмотрении", help_text="Введите статус заявки")
+    submission_date = models.DateField(auto_now_add=True)
+    request = models.ForeignKey('RepairRequest', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.comment_text} ({self.request.service})"
+
+
+class Comment(models.Model):
+    comment_text = models.CharField(max_length=200, help_text="Введите комментарий")
+    status = models.CharField(max_length=200, default="На рассмотрении", help_text="Введите статус заявки")
+    submission_date = models.DateField(auto_now_add=True)
+    service = models.ForeignKey('documentation.Service', on_delete=models.SET_NULL, null=True)
+    tenant = models.ForeignKey('documentation.Tenant', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.comment_text} ({self.service})"
+
+
+class Recommendation(models.Model):
+    service = models.ForeignKey('documentation.Service', on_delete=models.SET_NULL, null=True)
+    building = models.ForeignKey('documentation.Building', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.service} ({self.building})"
+
+
