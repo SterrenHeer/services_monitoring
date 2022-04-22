92e968568b3b52ff6666ba1e43061e85259fa2d7
SterrenHeer <sterrenheer@gmail.com>
2022-04-22 14:02:26 +0300

    Added creating and grouping feature for cleaning schedule

--- documentation/urls.py
+++ documentation/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('document/cleaning/schedule/', views.WorkWithCleaningSchedule.as_view(), name='cleaning_schedule'),
+    path('document/cleaning/schedule/', views.CleaningScheduleListView.as_view(), name='cleaning_schedule'),
     path('search/schedule/', views.SearchByCleaningSchedule.as_view(), name='search_schedule'),
+    path('schedule/item/create/', views.CreateScheduleItem.as_view(), name='schedule_item_create'),
 ]
--- documentation/views.py
+++ documentation/views.py
@@ -1,15 +1,17 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from .models import CleaningSchedule
 from django.db.models import Q
+import datetime
 
 
-class WorkWithCleaningSchedule(ListView):
+class CleaningScheduleListView(ListView):
     model = CleaningSchedule
     template_name = 'documentation/cleaning_schedule.html'
 
     def get_queryset(self):
         if self.request.user.groups.filter(name__in=['Master', 'Manager']).exists():
-            return CleaningSchedule.objects.all().order_by('date')
+            return CleaningSchedule.objects.all().order_by('building', 'date')
         elif self.request.user.groups.filter(name='Tenant').exists():
             return CleaningSchedule.objects.filter(building_id=self.request.user.tenant.apartment.building).order_by('date')
 
@@ -37,3 +39,23 @@ class SearchByCleaningSchedule(ListView):
         context = super().get_context_data(*args, **kwargs)
         context["search"] = f"search={self.request.GET.get('search')}&"
         return context
+
+
+class CreateScheduleItem(CreateView):
+    model = CleaningSchedule
+    template_name = 'documentation/create_schedule_item.html'
+    fields = ['building', 'service', 'worker', 'date', 'start_time']
+
+    def form_valid(self, form):
+        if form.cleaned_data['date'] < datetime.date.today():
+            form.add_error('date', 'Введите правильную дату.')
+        elif form.cleaned_data['date'] == datetime.date.today() and form.cleaned_data['start_time'] <= datetime.datetime.now().time():
+            form.add_error('start_time', 'Введите правильное время.')
+        if form.errors:
+            return self.form_invalid(form)
+
+        form.instance.user = self.request.user
+        return super(CreateScheduleItem, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('cleaning_schedule')
